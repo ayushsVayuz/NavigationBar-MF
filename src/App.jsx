d11814000dedd357541d121ae71fb9598411e476
Ayush Sharma <ayushs.sharma@vayuz.com>
2025-05-21 11:08:26 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import NavigationBar from './components/NavigationBar';
+
+const isStandalone = !window.__POWERED_BY_HOST__; 
+
+const App = () => {
+  const content = (
+    <div>
+      <NavigationBar />
+    </div>
+  );
+
+  return isStandalone ? <BrowserRouter>{content}</BrowserRouter> : content;
+};
+
+export default App;
---
+++ src/components/NavigationBar.jsx
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+import * as ReactRouterDom from 'react-router-dom';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+
+function NavigationBar() {
+  
+    const { Link} = ReactRouterDom
+
+    const [isOpen , setIsOpen] = useState (false)
+
+
+    
+    return (
+       
+            <nav className=" bg-blue-500 shadow-md p-4">
+              <div className="  max-w-6xl mx-auto flex flex-nowrap justify-between items-center">
+                <Link to="/home" className="text-2xl sm:text-3xl font-bold text-white">
+                  CRUD
+                </Link>
+
+       
+    
+                <button 
+                  onClick={() => setIsOpen(!isOpen)} 
+                  className="md:hidden p-2 text-white rounded hover:bg-white/20 focus:outline-none"
+                >
+                    <FontAwesomeIcon icon={faBars} size="lg" />
+                </button>
+        
+                <div className={`w-full md:flex md:w-auto ${isOpen ? "block" : "hidden"}`}>
+                  <ul className="flex flex-col md:flex-row md:space-x-8 text-lg text-white font-semibold">
+                    <li>
+                      <Link to="/home" className="block py-2 px-3 rounded">Home</Link>
+                      
+                    </li>
+                    <li>
+                      <Link to="/about" className="block py-2 px-3 rounded">About</Link>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </nav>
+          
+       
+    );
+}
+
+export default NavigationBar;
