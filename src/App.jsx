e0431fb8e693007e5efbe26114e0020ed78adda8
Ayush Sharma <ayushs.sharma@vayuz.com>
2025-05-21 14:53:26 +0530

    Resolved tailwind css error

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import NavigationBar from './components/NavigationBar';
+import "./index.css";
 
 const isStandalone = !window.__POWERED_BY_HOST__; 
 
