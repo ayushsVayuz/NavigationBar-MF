f2c84e13a93535caab420dab59c243ec6d73ea84
Ayush Sharma <ayushs.sharma@vayuz.com>
2025-05-22 08:09:31 +0530

    Adding No internet functionality

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,15 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import NavigationBar from './components/NavigationBar';
 import "./index.css";
+import CheckInternetConnection from './components/CheckInternetConnection';
 
 const isStandalone = !window.__POWERED_BY_HOST__; 
 
 const App = () => {
   const content = (
-    <div>
+    <CheckInternetConnection>
       <NavigationBar />
-    </div>
+    </CheckInternetConnection>
   );
 
   return isStandalone ? <BrowserRouter>{content}</BrowserRouter> : content;
